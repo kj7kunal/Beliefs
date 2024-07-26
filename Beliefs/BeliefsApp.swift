8575146b74cd0bf489f761911d261153ff2d402e
kj7kunal <kjkunal7996@gmail.com>
2024-07-26 17:17:28 +0900

    Add Xcode project structure and basic app files

---
+++ Beliefs/BeliefsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeliefsApp.swift
+//  Beliefs
+//
+//  Created by kunal.jain on 2024/07/26.
+//
+
+import SwiftUI
+
+@main
+struct BeliefsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
