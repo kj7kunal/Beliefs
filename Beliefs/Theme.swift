6f578766fe2ef9403ba5f13519943bce643c1d1c
kj7kunal <kjkunal7996@gmail.com>
2024-07-26 18:51:05 +0900

    setup basic theme

---
+++ Beliefs/Theme.swift
@@ -0,0 +1,15 @@
+//
+//  Theme.swift
+//  Beliefs
+//
+//  Created by kunal.jain on 2024/07/26.
+//
+
+import SwiftUI
+
+struct AppTheme {
+    static let primaryColor = Color.blue
+    static let secondaryColor = Color.gray
+    static let primaryFont = Font.system(size: 16, weight: .medium)
+    static let secondaryFont = Font.system(size: 14, weight: .regular)
+}
